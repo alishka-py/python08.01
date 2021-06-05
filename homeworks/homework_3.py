619cdc211c0941b4842714034de694259b3a9d57
ali <58395671+alishka-py@users.noreply.github.com>
2021-06-05 15:42:40 +0600

    Add files via upload

---
+++ homeworks/homework_3.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = input()
+num3 = int(input())
+
+if num2 == "+":
+    print(num1 + num3)
+elif num2 == "-":
+    print(num1 - num3)
+elif num2 == "*":
+    print(num1 * num3)
+elif num2 == "/":
+    print(num1 / num3)
